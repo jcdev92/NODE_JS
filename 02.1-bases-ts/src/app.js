27919f277b8a8209c7266ec43770380959d547c4
Jesus Clemente <jcdeveloper92@gmail.com>
2023-11-14 18:27:29 -0400

    clean app.js

---
+++ 02.1-bases-ts/src/app.js
@@ -0,0 +1,53 @@
+//? import - export
+    // const templateExports = require('./js-foundation/01-template')
+    // console.log(templateExports.emailTemplate);
+
+//? destructuring
+    // require('./js-foundation/02-destructuring')
+
+//? Callbacks
+    // const {getUserById} = require('./js-foundation/03-callbacks')
+
+    //? Arrow Callbacks
+    // const {getUserById} = require('./js-foundation/04-arrows')
+
+    // const id = 2;
+
+    //? old way 
+    // getUserById(id, function(error, user){
+    //     if (error) {
+    //         throw new Error(error);
+    //     }
+
+    //     console.log(user);
+    // })
+
+    //? arrows way
+    // getUserById(id, (error, user) => {
+    //     if (error) throw new Error(error);
+    //     console.log(user)
+    // })
+
+//? Factory Functions
+    // const {getId, getAge} = require('./plugins') //? patron adaptador
+    // const {buildMakePerson} = require('./js-foundation/05-factory')
+    // const makePerson = buildMakePerson({getId, getAge})
+
+
+    // const obj = {name: 'John', birthdate: '1985-10-21'};
+    // const john = makePerson(obj);
+    // console.log(john);
+
+//? promises, asyn await, http request...
+    // const getPokemonById = require('./js-foundation/06-promises');
+    // getPokemonById(4)
+    //   .then( ( pokemon ) => console.log({ pokemon }) )
+    //   .catch( ( err ) => console.log( err ) )
+    //   .finally( () => console.log('Finalmente') );
+
+//? winston logger
+    // const {buildLogger} = require('./plugins');
+    // const logger = buildLogger('./app.js');
+
+    // logger.log('Hola Mundo');
+    // logger.error('Aqui hubo un problema');
