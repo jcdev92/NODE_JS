14debf5683b468632e71048ecc252478878f6a32
Jesus Clemente <jcdeveloper92@gmail.com>
2023-11-20 17:50:37 -0400

    unit test 05 & 06

---
+++ 02.1-bases-ts/tests/05-factory.test.ts
@@ -0,0 +1,23 @@
+import { buildMakePerson } from "../src/js-foundation/05-factory";
+
+describe('../src/js-foundation/05-factory', () => {
+    const getId = () => '12345';
+    const getAge = () => 35;
+
+
+    test('buildMakePerson should return a function', () => {
+        const makePerson = buildMakePerson({getId, getAge})
+        expect(typeof makePerson).toBe('function')
+    });
+
+    test('buildMakePerson should return a person', () => {
+        const makePerson = buildMakePerson({getId, getAge})
+        const johnDoe = makePerson({name: 'John Doe', birthdate: '1985-10-21'})
+        expect(johnDoe).toEqual({
+            id: '12345',
+            name: 'John Doe',
+            birthdate: '1985-10-21',
+            age: 35
+        })
+    });
+})
