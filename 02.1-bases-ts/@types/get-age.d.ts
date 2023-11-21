659e91252acabbc139cee0716447083179d8868f
Jesus Clemente <jcdeveloper92@gmail.com>
2023-11-21 17:55:54 -0400

    reconfig some files, like get-id removed because dont have typescript support

--- 02.1-bases-ts/@types/get-age.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'get-age' {
-    // La función getAge recibe una fecha de nacimiento como string
-    // y devuelve un número que representa la edad en años
-    function getAge(birthDate: string): number;
-  
-    // Exporta la función getAge como el valor predeterminado del módulo
-    export default getAge;
-  }
-  
--- 02.1-bases-ts/src/plugins/get-age.plugin.ts
+++ 02.1-bases-ts/src/plugins/get-age.plugin.ts
@@ -1,9 +1,9 @@
 //* patron adaptador
 //? las dependencias tratar de que se usen a traves de funciones propias para que cuando toque hacer cambios de dependencias en toda la app, sea mas facil la adaptacion, solo se va a un solo sitio a cambiar.
 
-import getAgePlugin from 'get-age';
+// import getAgePlugin from 'get-age';
 
-export const getAge = (birthdate: string) => {
-    if (!birthdate) return new Error('birthdate is required');
-    return getAgePlugin(birthdate)
-}
+// export const getAge = (birthdate: string) => {
+//     if (!birthdate) return new Error('birthdate is required');
+//     return getAgePlugin(birthdate)
+// }
