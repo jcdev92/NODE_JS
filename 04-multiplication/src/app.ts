7da114e7708e56a2d72ee81e96fe8deb3cad4884
Jesus Clemente <jcdeveloper92@gmail.com>
2023-11-22 14:23:52 -0400

    give types tu variables

--- 04-multiplication/src/app.ts
+++ 04-multiplication/src/app.ts
@@ -1,17 +1,20 @@
 import fs from 'fs'
 
 // multiplication table
-let num: number = 10;
-const title = `=========== TABLA DEL ${num} ===========`;
-let content;
-let arr = [title]
+let num: number = 5;
+
+const title: string = `=========== TABLA DEL ${num} ===========`;
+let content:string;
+let arr: String[] = [title];
+
 console.log(title);
+
 for (let i: number = 0; i <= num; i++) {
     content = `            ${i} x ${num} = ${i * num}`;
     console.log(content);
-    arr.push(content)
+    arr.push(content);
 };
 
-const result = arr.join(' \n ')
-fs.writeFileSync('multiple.txt', result)
+const result: string = arr.join(' \n ');
+fs.writeFileSync('multiple.txt', result);
   
