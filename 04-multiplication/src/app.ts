8fe12b32cc9e8f414098004bd1a981b8621de0c6
Jesus Clemente <jcdeveloper92@gmail.com>
2023-11-22 15:03:30 -0400

    add output path and folder

--- 04-multiplication/src/app.ts
+++ 04-multiplication/src/app.ts
@@ -3,18 +3,24 @@ import fs from 'fs'
 // multiplication table
 let num: number = 5;
 
-const title: string = `=========== TABLA DEL ${num} ===========`;
-let content:string;
+const title: string = `
+====================== 
+    TABLA DEL ${num} 
+======================`;
+
+let content: string;
 let arr: String[] = [title];
 
 console.log(title);
 
 for (let i: number = 0; i <= num; i++) {
-    content = `            ${i} x ${num} = ${i * num}`;
+    content = `    ${i} x ${num} = ${i * num}`;
     console.log(content);
     arr.push(content);
 };
 
 const result: string = arr.join(' \n ');
-fs.writeFileSync('multiple.txt', result);
-  
+
+const outputPath = `outputs/folder1`;
+fs.mkdirSync(outputPath, { recursive: true });
+fs.writeFileSync(`${outputPath}/tabla-${num}.txt`, result);
