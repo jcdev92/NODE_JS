1f24bf4088039c42bd99ee32062ec38b260df172
Jesus Clemente <jcdeveloper92@gmail.com>
2023-12-08 12:52:14 -0400

    variables de entorno configuration

---
+++ 05-NOC/src/config/plugins/envs.plugin.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import * as env from 'env-var';
+
+//? validar variables de entorno mediante env-var
+export const envs = {
+    PORT: env.get('PORT').required().asPortNumber(),
+    MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
+    MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
+    PROD: env.get('PROD').required().asBool(),
+}
