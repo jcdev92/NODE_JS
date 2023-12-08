16fe4a6f86a31b48cdfc638bb60139b19bcee9ea
Jesus Clemente <jcdeveloper92@gmail.com>
2023-12-08 12:57:51 -0400

    config env

--- 05-NOC/src/app.ts
+++ 05-NOC/src/app.ts
@@ -1,4 +1,6 @@
 import { Server } from './presentation/server';
+import { envs } from './config/plugins/envs.plugin';
+
 
 (async () => {
     main();
@@ -6,4 +8,5 @@ import { Server } from './presentation/server';
 
 function main() {
     Server.start();
+    // console.log(envs);
 };
